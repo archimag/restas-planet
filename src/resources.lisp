62b34a12cb628f4cc635454504b17cc0b8cf0960
Moskvitin Andrey <archimag@gmail.com>
2009-06-23 16:53:01 +0400

    update

---
+++ src/resources.lisp
@@ -0,0 +1,21 @@
+;;; resources.lisp
+
+(defpackage :planet.resource
+  (:use :cl)
+  (:export :css-file
+           :feed-icon/10x10
+           :feed-icon/14x14))
+
+(in-package :planet.resource)
+
+(defparameter *resource-dir* (merge-pathnames "resources/"
+                                              (asdf:component-pathname (asdf:find-system :planet))))
+
+(defun css-file ()
+  (merge-pathnames "planet.css" *resource-dir*))
+
+(defun feed-icon/10x10 ()
+  (merge-pathnames "feed-icon-10x10.png" *resource-dir*))
+
+(defun feed-icon/14x14 ()
+  (merge-pathnames "feed-icon-10x10.png" *resource-dir*))
